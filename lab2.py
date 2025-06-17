4d673a36ebe2ee44cc2578e62026ab5fabd5af12
Nelson-Ramos321 <nelson.ramosgonon@gmail.com>
2025-06-17 12:10:27 -0700

    completed lab 2

---
+++ lab2.py
@@ -0,0 +1,6 @@
+name = "Nelson Ramos"
+nickname = "Nelson"
+age = 14
+used_python = False
+hobbies = ["sleep","play","eat"]
+favorite_things = {"game": "brawl stars", "food": "pizza", "color": "Blue"}
